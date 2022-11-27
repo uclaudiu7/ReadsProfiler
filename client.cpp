fb659db6c2e680dd3d19127be304064e11e35391
Claudiu <ursescuclaudiu7@gmail.com>
2022-11-27 20:08:43 +0200

    made server-client connection

--- client.cpp
+++ client.cpp
@@ -1,12 +1,59 @@
-#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
 
-using namespace std;
+#define PORT 2024
 
-int main(){
-    cout << "yes\n";
-    int client;
-    client = 2;
-    client++;
-    
+int main(int argc, char *argv[])
+{
+    int sd;
+    struct sockaddr_in server; // structura folosita pentru conectare
+    char msg[100];             // mesajul trimis
+
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Eroare la socket().\n");
+        return errno;
+    }
+
+    bzero((char *) &server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr("0.0.0.0");
+    server.sin_port = htons(PORT);
+
+    if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+        perror("[client]Eroare la connect().\n");
+        return errno;
+    }
+
+    while(1){
+        bzero(msg, 100);
+        read(0, msg, 100);
+
+        if (write(sd, msg, 100) <= 0){
+            perror("[client]Eroare la write() spre server.\n");
+            return errno;
+        }
+
+        /* citirea raspunsului dat de server
+        (apel blocant pana cand serverul raspunde); Atentie si la cum se face read- vezi cursul! */
+
+        bzero(msg, 100);
+        if (read(sd, msg, 100) < 0){
+            perror("[client]Eroare la read() de la server.\n");
+            return errno;
+        }
+
+        printf("[server]--> %s", msg);
+        if(strncmp("stop", msg, 4) == 0)
+            break;
+    }
+    close(sd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,11 +1,76 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 using namespace std;
 
+#define PORT 2024
+
 int main(){
-    cout << "yes\n";
-    char server;
-    server = 'a';
-    
+    struct sockaddr_in server;
+    struct sockaddr_in from;
+    char msg[100];
+    char rasp[100]= " ";
+    int sd;
+
+    if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("[server]Eroare la socket().\n");
+        return errno;
+    }
+
+    bzero(&server, sizeof(server));   // preparing data structures
+    bzero(&from, sizeof(from));
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(PORT);
+
+    if(bind(sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1){
+        perror("[server]Eroare la bind().\n");
+        return errno;
+    }
+
+    if(listen(sd, 5) == -1){
+        perror("[server]Eroare la listen().\n");
+        return errno;
+    }
+
+    int client;
+    int length = sizeof(from);
+    printf("[server]Asteptam la portul %d...\n", PORT);
+    fflush(stdout);
+
+    client = accept(sd, (struct sockaddr *) &from, (socklen_t *) &length);
+
+    if(client < 0){
+        perror("[server]Eroare la accept().\n");
+    }
+
+    while(1){
+        bzero(msg, 100);
+
+        if(read(client, msg, 100) <= 0){
+            perror("[server]Eroare la read() de la client.\n");
+        }
+
+        printf("[client]--> %s", msg);
+
+        bzero(msg, 100);
+        read(0, msg, 100);
+
+        if(write(client, msg, strlen(msg)) <= 0){
+            perror("[server]Eroare la write() catre client.\n");
+        }
+        if(strncmp("stop", msg, 4) == 0)
+            break;
+    }
+    close(client);
+    close(sd);
     return 0;
 }
